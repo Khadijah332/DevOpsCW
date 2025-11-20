96a23b6d14389e7334e5c68d60b9d134497a7807
Khadijah <121125637+Khadijah332@users.noreply.github.com>
2025-11-20 16:20:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 weather_classes = ['clear', 'cloudy', 'drizzly', 'foggy', 'hazey', 'misty', 'rainy', 'smokey', 'thunderstorm']
 
 def load_model(model_path = 'model/model.pkl'):
-	return pickle.load(open(model_path, 'rb'))
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+return model
 
 def classify_weather(features):
 	model = load_model()
